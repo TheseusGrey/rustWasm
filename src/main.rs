027e72db0975bb7c47ca5ed6fc95db3e6f47e63d
Paris <parisgoldmansmith@googlemail.com>
2023-01-18 20:42:44 +0000

    okay, got an idea for a macro to make life easier

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use actix_web::{App, HttpServer};
 mod services;
 
-use services::example::{
-    example_service,
-};
+use services::example::example_service;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-        .service(example_service())
-    )
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(example_service()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
--- src/services/example.rs
+++ src/services/example.rs
@@ -1,12 +1,12 @@
 use actix_web::{get, post, web, HttpResponse, Responder, Scope};
 
 pub fn example_service() -> Scope {
-    web::scope("/example")
-      .service(hello)
-      .service(echo)
+    web::scope("/example").service(hello).service(echo)
 }
 
-#[get("/")]
+// Could build a macro to generate a simple CRUD API for me?
+
+#[get("")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
